4b05c2ae5c5fc3aed41954d48727eef9502c92ee
rafaelsene01 <rafaelssene@gmail.com>
2025-05-11 00:46:07 -0300

    update config

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,14 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  // logging: {
+  //   fetches: { fullUrl: true }
+  // }
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '100mb',
+    },
+  },
 };
 
 export default nextConfig;
